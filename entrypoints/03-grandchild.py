4bb732d68c5723d684a28d5a57369c34e1d67a7c
Patricio-Andre <a260363@dac.unicamp.br>
2026-05-08 23:33:18 -0300

    50% das cenas concluídas

--- entrypoints/03-grandchild.py
+++ entrypoints/03-grandchild.py
@@ -15,9 +15,27 @@ if __name__ == "__main__":
     runtime = urenderer.application.Runtime(renderer, name="03-grandchild")
 
     # Crie a cena
+    deslocamento_base = -3
+    # Criando o Pai
+    pai = urenderer.node.Node()
+    pai.translation = np.array([0, 0, deslocamento_base], np.float64)
+    pai.render_data = urenderer.geometry.polygonal_ifs.get_ifs_cube()
+    # Criando o Filho
+    filho = urenderer.node.Node()
+    filho.translation = np.array([0, 0, deslocamento_base], np.float64)
+    filho.render_data = urenderer.geometry.polygonal_ifs.get_ifs_cube()
+    # Criando o Neto
+    neto = urenderer.node.Node()
+    neto.translation = np.array([0, 0, deslocamento_base], np.float64)
+    neto.render_data = urenderer.geometry.polygonal_ifs.get_ifs_cube()
+    
+    pai.add_child(filho)
+    filho.add_child(neto)
+    runtime.scene.add_child(pai)
 
     runtime.iter(capture=True)
 
     # Rotacione o nó avô
-
+    pai.rotation = np.array([0, 30, 0], np.float64)
+    
     runtime.iter(capture=True)
